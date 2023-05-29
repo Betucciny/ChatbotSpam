c119ad0c4a1f0bfeaa1b11d60367e07d372a92ee
betucciny <betohr48@hotmail.com>
2023-05-29 01:24:17 -0600

    yei

--- spam/spamDetectorTrain.py
+++ spam/spamDetectorTrain.py
@@ -23,7 +23,6 @@ maxtokens = 5000  # the maximum number of tokens per document
 maxtokenlen = 500  # the maximum length of each token
 
 physical_devices = tf.config.experimental.list_physical_devices('GPU')
-print(physical_devices)
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 
